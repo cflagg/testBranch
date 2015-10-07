303563e4b30013fb16de48e887cb617ef6bc18f5
cflagg <cflagg@neoninc.org>
2015-10-06 20:59:03 -0600

    branch 2 commit

--- test_m1.py
+++ test_m1.py
@@ -14,3 +14,6 @@ import os,glob, re
 ### This is a test branch 1 ### 3 lines for test branch 1 edit, 8:56 pm
 ### This is a test branch 1 ###
 
+### This is a test branch 2 ###
+### This is a test branch 2 ### 3 lines for test branch 2 edit, 8:58 pm
+### This is a test branch 2 ###
